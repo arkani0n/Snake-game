afa4c575debf1c1ab1919170af5ec9077720c0b4
arkani0n <vladssan@gmail.com>
2020-10-11 20:22:15 +0300

    saving user progress

--- snake_game.py
+++ snake_game.py
@@ -21,14 +21,15 @@ def draw(canvas, list, color):
 def change_level():
     global in_game
     global current_level
+    curent_level_index=switch_variable.get()
     in_game = False
-    current_level = Levels[switch_variable.get()]
-    restart(user_snake, current_level, switch_variable.get())
+    current_level = Levels[curent_level_index]
+    restart(user_snake, current_level, curent_level_index)
     in_game = True
-    play(root, canvas, current_level, user_snake)
+    play(root, canvas, current_level, user_snake,curent_level_index)
 
 
-def play(root, canvas, current_level, user_snake):
+def play(root, canvas, current_level, user_snake,level_index):
     global in_game
     end = 0
 
@@ -40,11 +41,17 @@ def play(root, canvas, current_level, user_snake):
         for list, color in [[current_level.food, 'green'],
                             [current_level.walls, 'blue'],
                             [current_level.bombs, 'red'],
+                            [current_level.win_point, 'yellow'],
                             [user_snake.snake, 'black']]:
             draw(canvas, list, color)
         if len(user_snake.snake) == 0 or (user_snake.eror() or user_snake.snake[0] in current_level.walls):
             in_game = False
         else:
+            if user_snake.snake[0] in current_level.win_point:
+                progress[str(level_index+1)]='1'
+                update_progress()
+                place_level_button()
+                return
             user_snake.move()
             current_level.spawn_food()
             if user_snake.snake[0] in current_level.food:
@@ -80,11 +87,12 @@ canvas.pack(side=tkinter.BOTTOM)
 in_game = False
 user_snake = classes.Snake(root)
 current_level = None
+progress = {}
+
 
 # ========== Progress
 
 def place_level_button():
-    progress={}
     with open('user_progress.txt', 'r+') as file:
         for i in file.readlines():
             i=i.strip().split('=')
@@ -93,14 +101,17 @@ def place_level_button():
         for i in for_buttons:
             if i[0] in allready_placed:
                 continue
-            if int(progress[i[0]]):
-                print(((progress[i[0]])))
+            if int(progress[str(i[2])]):
                 tkinter.Radiobutton(root, text=i[0], variable=switch_variable,
                             indicatoron=False, value=i[2], width=8,
                             command=change_level).place(anchor=tkinter.NW, rely=i[1])
                 allready_placed.append(i[2])
 
-
+def update_progress():
+    with open('user_progress.txt','w') as file:
+        for i in progress:
+            file.write(i+'='+progress[i])
+            file.write('\n')
 place_level_button()
 # ==========Progres
 root.mainloop()
